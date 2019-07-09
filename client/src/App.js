0ba1b4c49ee92ee2089b66e28036ffb134844ba3
Akash Chakroborty <akashranjanchakroborty@gmail.com>
2019-07-09 21:24:03 +0530

    configuring react router for navbar and landing

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,22 @@
-import React, {Fragment} from 'react';
+import React from 'react';
 import Navbar from './components/layout/Navbar';
 import Landing from "./components/layout/Landing";
+import Login from './components/auth/Login';
+import Register from "./components/auth/Register";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
 
 const App = () => (
-    <Fragment>
-      <Navbar />
-      <Landing />
-    </Fragment>
+  <Router>
+    <Navbar />
+    <Route exact path="/" component= {Landing} />
+    <section className="container">
+      <Switch>
+        <Route exact path ='/register' component= {Register} />
+        <Route exact path ='/login' component= {Login} />
+      </Switch>
+    </section>
+  </Router>
   );
 
 export default App;
