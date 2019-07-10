60783244ae9bd511780065d8ef80106ce68b9d2e
Akash Chakroborty <akashranjanchakroborty@gmail.com>
2019-07-11 01:40:04 +0530

    redux setup

--- client/src/App.js
+++ client/src/App.js
@@ -4,9 +4,14 @@ import Landing from "./components/layout/Landing";
 import Login from './components/auth/Login';
 import Register from "./components/auth/Register";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+//redux
+import { Provider } from 'react-redux';
+import store from './store';
+
 import './App.css';
 
 const App = () => (
+  <Provider store={store}>
   <Router>
     <Navbar />
     <Route exact path="/" component= {Landing} />
@@ -17,6 +22,7 @@ const App = () => (
       </Switch>
     </section>
   </Router>
+  </Provider>
   );
 
 export default App;
