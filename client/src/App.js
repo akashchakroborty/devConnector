30f0ede1b215c2d45d8a6326cead8c82bf4b6760
Akash Chakroborty <akashranjanchakroborty@gmail.com>
2019-07-13 20:10:48 +0530

    load user action creator

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Navbar from './components/layout/Navbar';
 import Landing from "./components/layout/Landing";
 import Login from './components/auth/Login';
@@ -9,9 +9,21 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Provider } from 'react-redux';
 import store from './store';
 
+import setAuthToken from './utils/setAuthToken';
+import {loadUser} from './actions/auth';
+
 import './App.css';
 
-const App = () => (
+if(localStorage.token) {
+  setAuthToken(localStorage.token);
+}
+
+const App = () => { 
+  useEffect(() => {
+    store.dispatch(loadUser());
+    }, []);
+
+  return (
   <Provider store={store}>
   <Router>
     <Navbar />
@@ -25,6 +37,7 @@ const App = () => (
     </section>
   </Router>
   </Provider>
-  );
+  )
+};
 
 export default App;
