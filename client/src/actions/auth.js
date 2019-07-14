0688d66967c8358ce5de90cae17d744672c6d9b0
Akash Chakroborty <akashranjanchakroborty@gmail.com>
2019-07-14 18:19:05 +0530

    implement logout mechanism

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,4 +1,12 @@
-import { REGISTER_SUCCESS , REGISTER_FAIL, USER_LOADED, AUTH_ERROR, LOGIN_SUCCESS, LOGIN_FAIL } from './types';
+import {
+    REGISTER_SUCCESS,
+    REGISTER_FAIL,
+    USER_LOADED,
+    AUTH_ERROR,
+    LOGIN_SUCCESS,
+    LOGIN_FAIL,
+    LOGOUT
+} from './types';
 import axios from 'axios';
 import { setAlert } from './alert';
 import setAuthToken from '../utils/setAuthToken';
@@ -72,3 +80,11 @@ export const login = (email, password) => async dispatch => {
       });
     }
   }
+
+//  Logout / clear Profile
+
+export const logout = () => async dispatch => {
+    dispatch({
+        type: LOGOUT
+    });
+}
