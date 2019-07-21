a9584dfc04e81b92d676b3d4b7335d4deb8ce080
Akash Chakroborty <akashranjanchakroborty@gmail.com>
2019-07-21 14:52:52 +0530

    added post item page with likes and dislikes in individul post

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 import { setAlert } from './alert';
 import {
     GET_POSTS,
-    POST_ERROR
+    POST_ERROR,
+    UPDATE_LIKES
   } from './types';
 
 // Get posts
@@ -20,4 +21,39 @@ export const getPosts = () => async dispatch => {
         payload: { msg: err.response.statusText, status: err.response.status }
       });
     }
-  };  
+};
+
+// Add like
+export const addLike = id => async dispatch => {
+    try {
+      const res = await axios.put(`/api/posts/like/${id}`);
+  
+      dispatch({
+        type: UPDATE_LIKES,
+        payload: { id, likes: res.data }
+      });
+    } catch (err) {
+      dispatch({
+        type: POST_ERROR,
+        payload: { msg: err.response.statusText, status: err.response.status }
+      });
+    }
+  };
+  
+  // Remove like
+  export const removeLike = id => async dispatch => {
+    try {
+      const res = await axios.put(`/api/posts/unlike/${id}`);
+  
+      dispatch({
+        type: UPDATE_LIKES,
+        payload: { id, likes: res.data }
+      });
+    } catch (err) {
+      dispatch({
+        type: POST_ERROR,
+        payload: { msg: err.response.statusText, status: err.response.status }
+      });
+    }
+  };
+  
--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -14,3 +14,4 @@ export const UPDATE_PROFILE = 'UPDATE_PROFILE';
 export const ACCOUNT_DELETED = 'ACCOUNT_DELETED';
 export const GET_POSTS = 'GET_POSTS';
 export const POST_ERROR = 'POST_ERROR';
+export const UPDATE_LIKES = 'UPDATE_LIKES';
