ab1c1aee0647c42a7ea748477b1a546c54b7c1af
Akash Chakroborty <akashranjanchakroborty@gmail.com>
2019-07-22 20:36:00 +0530

    completing actions and reducers for profiles

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -20,3 +20,5 @@ export const DELETE_POST = 'DELETE_POST';
 export const ADD_POST = 'ADD_POST';
 export const ADD_COMMENT = 'ADD_COMMENT';
 export const REMOVE_COMMENT = 'REMOVE_COMMENT';
+export const GET_PROFILES = 'GET_PROFILES';
+export const GET_REPOS = 'GET_REPOS';
