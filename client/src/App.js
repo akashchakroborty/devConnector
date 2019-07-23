883fc4bfbb4552f4d77ebc0e4292bbb01a7d4d06
Akash Chakroborty <akashranjanchakroborty@gmail.com>
2019-07-23 21:50:52 +0530

    single profile page

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import EditProfile from "./components/profile-forms/EditProfile";
 import AddEducation from "./components/profile-forms/AddEducation";
 import AddExperience from "./components/profile-forms/AddExperience";
 import Profiles from './components/profiles/Profiles';
+import Profile from './components/profile/Profile';
 import Posts from "./components/posts/Posts";
 import Post from "./components/post/Post";
 import PrivateRoute from "./components/routing/PrivateRoute";
@@ -43,6 +44,7 @@ const App = () => {
         <Route exact path ='/register' component= {Register} />
         <Route exact path ='/login' component= {Login} />
         <Route exact path='/profiles' component={Profiles} />
+        <Route exact path='/profile/:id' component={Profile} />
         <PrivateRoute exact path ='/dashboard' component= {Dashboard} />
         <PrivateRoute exact path ='/create-profile' component= {CreateProfile} />
         <PrivateRoute exact path ='/edit-profile' component= {EditProfile} />
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -33,7 +33,6 @@ const Navbar = ({auth: { isAuthenticated, loading }, logout}) => {
             <li><Link to='/profiles'>Developers</Link></li>
             <li><Link to="/register">Register</Link></li>
             <li><Link to="/login">Login</Link></li>
-            <li><a href='#!'>Developers</a></li>
         </ul>
     );
     return (
