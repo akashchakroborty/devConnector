9d1ad8f57791f56bf93f499d73f4cbb855fd7127
Akash Chakroborty <akashranjanchakroborty@gmail.com>
2019-07-24 20:48:23 +0530

    added priofile top and about section

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import { getProfileById } from '../../actions/profile';
+import ProfileTop from './ProfileTop';
+import ProfileAbout from './PrifileAbout';
 
 const Profile = ({
   getProfileById,
@@ -31,6 +33,10 @@ const Profile = ({
                 Edit Profile
               </Link>
           )}
+          <div className='profile-grid my-1'>
+            <ProfileTop profile={profile} />
+            <ProfileAbout profile={profile} />
+            </div>
         </Fragment>
       )}
     </Fragment>
