4c2b1d8f59088e8669dbc78d468fa8990f01011d
Akash Chakroborty <akashranjanchakroborty@gmail.com>
2019-07-24 21:19:23 +0530

    add education and experience in profile page

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -3,9 +3,11 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
-import { getProfileById } from '../../actions/profile';
 import ProfileTop from './ProfileTop';
-import ProfileAbout from './PrifileAbout';
+import ProfileAbout from './ProfileAbout';
+import ProfileExperience from './ProfileExperience';
+import ProfileEducation from './ProfileEducation';
+import { getProfileById } from '../../actions/profile';
 
 const Profile = ({
   getProfileById,
@@ -32,11 +34,42 @@ const Profile = ({
               <Link to='/edit-profile' className='btn btn-dark'>
                 Edit Profile
               </Link>
-          )}
+            )}
           <div className='profile-grid my-1'>
             <ProfileTop profile={profile} />
             <ProfileAbout profile={profile} />
+            <div className='profile-exp bg-white p-2'>
+              <h2 className='text-primary'>Experience</h2>
+              {profile.experience.length > 0 ? (
+                <Fragment>
+                  {profile.experience.map(experience => (
+                    <ProfileExperience
+                      key={experience._id}
+                      experience={experience}
+                    />
+                  ))}
+                </Fragment>
+              ) : (
+                <h4>No experience credentials</h4>
+              )}
             </div>
+
+            <div className='profile-edu bg-white p-2'>
+              <h2 className='text-primary'>Education</h2>
+              {profile.education.length > 0 ? (
+                <Fragment>
+                  {profile.education.map(education => (
+                    <ProfileEducation
+                      key={education._id}
+                      education={education}
+                    />
+                  ))}
+                </Fragment>
+              ) : (
+                <h4>No education credentials</h4>
+              )}
+            </div>
+          </div>
         </Fragment>
       )}
     </Fragment>
